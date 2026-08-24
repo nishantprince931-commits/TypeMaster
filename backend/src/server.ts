9a966215af728cedd05b3f3923860621383bea7a
Nishant Singh <nishantprince931@gmail.com>
2026-08-24 07:52:13 +0530

    Fix CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,14 +15,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://type-master-steel-three.vercel.app",
-    ],
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
